c8f14fee33b8e607cbead934a4cdcfa585b85b64
saros <saros@LAPTOP-JQTANCVI>
2022-12-31 18:29:20 +0200

    Add project files.

---
+++ Sarosi_Lucian_Lab11/Data/IRestService.cs
@@ -0,0 +1,12 @@
+﻿using System.Threading.Tasks;
+using Sarosi_Lucian_Lab11.Models;
+
+namespace Sarosi_Lucian_Lab11.Data
+{
+    public interface IRestService
+    {
+        Task<List<ShopList>> RefreshDataAsync();
+        Task SaveShopListAsync(ShopList item, bool isNewItem);
+        Task DeleteShopListAsync(int id);
+    }
+}
